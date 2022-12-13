5a33f9393c4104dbac8b9dd4734b4caf452dc304
Alexander Kornilov <kas_invader@yandex.ru>
2022-12-13 22:36:08 +0500

    correction_3

--- Продвинутая выборка данных/SELECT_2.sql
+++ Продвинутая выборка данных/SELECT_2.sql
@@ -13,15 +13,15 @@ WHERE yer >= 2019 AND yer <= 2020;
 --средняя продолжительность треков по каждому альбому
 SELECT a.name, AVG(t.duration_s) FROM album a
 JOIN track t ON t.album_id = a.id
-GROUP by a.name
+GROUP by a.name 
 ORDER BY AVG(t.duration_s)
 
 --все исполнители, которые не выпустили альбомы в 2020 году
-SELECT e.name FROM executor e
+SELECT DISTINCT e.name FROM executor e
 JOIN executoralbum ea ON e.id = ea.executor_id
 JOIN album a ON ea.album_id = a.id
-WHERE a.yer != 2020
-GROUP BY e.name;
+WHERE e.name NOT IN (SELECT e.name FROM executor WHERE a.yer = 2020);
+
 
 --названия сборников, в которых присутствует конкретный исполнитель (выберите сами)
 SELECT c.name FROM collection c
@@ -30,8 +30,7 @@ JOIN track t ON tc.track_id = t.id
 JOIN album a ON t.album_id = a.id
 JOIN executoralbum ea ON a.id = ea.album_id 
 LEFT JOIN executor e ON ea.executor_id = e.id 
-WHERE e.name = 'Beatles'
-GROUP BY c.name;
+WHERE e.name = 'Beatles';
 
 --название альбомов, в которых присутствуют исполнители более 1 жанра
 SELECT a.name FROM album a
@@ -53,16 +52,18 @@ SELECT e.name FROM executor e
 JOIN executoralbum ea ON e.id = ea.executor_id
 JOIN album a ON ea.album_id = a.id
 JOIN track t ON a.id = t.album_id
-GROUP BY e.name, t.duration_s
-HAVING t.duration_s = (SELECT MIN(duration_s) FROM track)
-ORDER BY e.name
+WHERE t.duration_s = (SELECT MIN(duration_s) FROM track)
 
 --название альбомов, содержащих наименьшее количество треков
 SELECT a.name FROM album a
 JOIN track t ON a.id = t.album_id
 GROUP BY a.name
-ORDER BY COUNT(t.id)
-LIMIT 1
+HAVING COUNT(*) = (SELECT COUNT(*) FROM album a
+					JOIN track t ON a.id = t.album_id
+					GROUP BY a.name	
+					LIMIT 1);
+
+
 
 
 
